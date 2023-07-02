5144e4e79d9b00489b73fd2d486443c231d0a1be
Tony Gorez <gorez.tony@gmail.com>
2023-07-02 17:10:49 +0200

    chore: format code

--- test/region_test.cc
+++ test/region_test.cc
@@ -14,10 +14,10 @@ TEST(region, init_region) {
 
 TEST(region, alloc_region) {
   Region *r = init_region(1024);
-  const char* string = "Hello, World!";
+  const char *string = "Hello, World!";
   const size_t str_size = strlen(string) + 1;
 
-  char* alloc_str = (char*)alloc_region(r, str_size); 
+  char *alloc_str = (char *)alloc_region(r, str_size);
   strcpy(alloc_str, string);
 
   EXPECT_STREQ(alloc_str, string);
@@ -30,14 +30,14 @@ TEST(region, alloc_region) {
 TEST(region, alloc_region_multiple) {
   Region *r = init_region(1024);
 
-  const char* string = "Hello, World!";
+  const char *string = "Hello, World!";
   const size_t str_size = strlen(string) + 1;
-  char* alloc_str = (char*)alloc_region(r, str_size); 
+  char *alloc_str = (char *)alloc_region(r, str_size);
   strcpy(alloc_str, string);
 
-  const char* string2 = "Hello, World! 2";
+  const char *string2 = "Hello, World! 2";
   const size_t str_size2 = strlen(string2) + 1;
-  char* alloc_str2 = (char*)alloc_region(r, str_size2); 
+  char *alloc_str2 = (char *)alloc_region(r, str_size2);
   strcpy(alloc_str2, string2);
 
   EXPECT_STREQ(alloc_str, string);
@@ -49,11 +49,11 @@ TEST(region, alloc_region_multiple) {
 }
 
 TEST(region, alloc_region_overflow) {
-  const char* string = "Hello, World!";
+  const char *string = "Hello, World!";
   const size_t str_size = strlen(string) + 1;
   Region *r = init_region(str_size - 1);
 
-  void* p = alloc_region(r, str_size);
+  void *p = alloc_region(r, str_size);
 
   EXPECT_EQ(p, nullptr);
   EXPECT_EQ(r->used, 0);
@@ -63,16 +63,16 @@ TEST(region, alloc_region_overflow) {
 }
 
 TEST(region, alloc_region_multiple_overflow) {
-  const char* string = "Hello, World!";
+  const char *string = "Hello, World!";
   const size_t str_size = strlen(string) + 1;
   Region *r = init_region(str_size + 1);
 
-  char* alloc_str = (char*)alloc_region(r, str_size);
+  char *alloc_str = (char *)alloc_region(r, str_size);
   strcpy(alloc_str, string);
 
-  const char* string2 = "boom";
+  const char *string2 = "boom";
   const size_t str_size2 = strlen(string2) + 1;
-  char* alloc_str2 = (char*)alloc_region(r, str_size2);
+  char *alloc_str2 = (char *)alloc_region(r, str_size2);
 
   EXPECT_STREQ(alloc_str, string);
   EXPECT_EQ(alloc_str2, nullptr);
@@ -84,12 +84,11 @@ TEST(region, alloc_region_multiple_overflow) {
 TEST(region, region_free) {
   Region *r = init_region(1024);
 
-  const char* string = "Hello, World!";
+  const char *string = "Hello, World!";
   const size_t str_size = strlen(string) + 1;
-  char* alloc_str = (char*)alloc_region(r, str_size); 
+  char *alloc_str = (char *)alloc_region(r, str_size);
   strcpy(alloc_str, string);
 
   free_region(r);
   r = nullptr;
 }
-
